f89b5f99efc92f16a31f022a28f8262d391f69d3
Greg Cain <codegregcode@gmail.com>
2022-12-05 21:43:58 +0000

    completes first kata

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,19 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+const divide3 = (number % 3) === 0;
+const divide5 = (number % 5) === 0;
+
+if (divide3 && divide5) {
+    return 'FizzBuzz'
+}
+
+if (divide3) {
+    return 'Fizz'
+}
+
+if (divide5) {
+    return 'Buzz'
+}
+return number
+}
 
 module.exports = fizzBuzz;
