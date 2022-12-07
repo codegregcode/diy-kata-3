27be57d2fc928e492add6255a5cbe2cc29acb397
Greg Cain <codegregcode@gmail.com>
2022-12-07 16:33:45 +0000

    writes additional tests for function

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -4,4 +4,12 @@ describe("humanCatDogYears", () => {
   test("returns human years", () => {
     expect(humanCatDogYears(10)).toStrictEqual([10]);
   });
+
+  xtest("returns human years and cat years", () => {
+    expect(humanCatDogYears(10)).toStrictEqual([10, 56]);
+  });
+
+  xtest("returns human, cat and dog years", () => {
+    expect(humanCatDogYears(10)).toStrictEqual([10, 56, 64]);
+  });
 });
