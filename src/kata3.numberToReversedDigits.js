a1bc1eb647e0febe8bb858b5b0a7c1f1b000cc5d
Greg Cain <codegregcode@gmail.com>
2022-12-07 16:05:35 +0000

    completes kata3

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    return number.toString().split('').reverse().map(digit => parseInt(digit));
+};
+// convert number to string number.toString().split('').reverse()
+// convert strings to numbers
+// add to array
+// return array
 
 module.exports = numberToReversedDigits;
