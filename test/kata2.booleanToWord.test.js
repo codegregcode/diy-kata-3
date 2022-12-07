f387f8d84a6b51ab392d6f720eacbe8a89fa504c
Greg Cain <codegregcode@gmail.com>
2022-12-07 12:15:39 +0000

    initialises 2nd test passes kata

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -5,7 +5,7 @@ describe("booleanToWord", () => {
   test("returns Yes when passed true", () => {
     expect(booleanToWord(true)).toBe('Yes');
   });
-  xtest("return No when passed false", () => {
+  test("return No when passed false", () => {
     expect(booleanToWord(false)).toBe('No');
   });
 });
