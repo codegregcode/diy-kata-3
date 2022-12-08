06ea6410957f24acb76aceace7bb7417f971c537
Greg Cain <codegregcode@gmail.com>
2022-12-08 15:51:50 +0000

    writes test for kata6

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,10 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const names = [{name: 'Bart'}, {name: 'Lisa'}, {name:'Maggie'}];
+    expect(joinNames(names)).toBe('Bart, Lisa & Maggie');
+    const names2 = [{name: 'Bob'}, {name: 'Linda'}, {name: 'Tina'}, {name: 'Gene'}, {name: 'Louise'}];
+    expect(joinNames(names2)).toBe('Bob, Linda, Tina, Gene & Louise');
+  });
 });
