0c20ac6c9c665083ddbd965f1fc7f12499a7254b
Greg Cain <codegregcode@gmail.com>
2022-12-08 10:26:28 +0000

    rewrites tests

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,15 +1,11 @@
 const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 describe("humanCatDogYears", () => {
-  test("returns human years", () => {
-    expect(humanCatDogYears(10)).toStrictEqual([10]);
-  });
-
-  xtest("returns human years and cat years", () => {
-    expect(humanCatDogYears(10)).toStrictEqual([10, 56]);
-  });
-
-  xtest("returns human, cat and dog years", () => {
+  test("returns human, cat and dog years", () => {
     expect(humanCatDogYears(10)).toStrictEqual([10, 56, 64]);
+    expect(humanCatDogYears(1)).toStrictEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toStrictEqual([2, 24, 24]);
+    expect(humanCatDogYears(18)).toStrictEqual([18, 88, 104]);
+    expect(humanCatDogYears(22)).toStrictEqual([22, 104, 124]);
   });
 });
