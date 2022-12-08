5c54d6ab493860d67aabd3ef6377de1a8f7725eb
Greg Cain <codegregcode@gmail.com>
2022-12-08 10:37:48 +0000

    completes kata4

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,15 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  const catYears = (number - 2) * 4 + 24;
+  const dogYears = (number - 2) * 5 + 24;
+  return  number === 1 ? [number, 15, 15] : number === 2 ? [number, 24, 24] : [number, catYears, dogYears];
+
+  //   if (number === 1) {
+//     return [number, 15, 15];
+//   }
+//   if (number === 2) {
+//     return [number, 24, 24];
+//   }
+//   return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
