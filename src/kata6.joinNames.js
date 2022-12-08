dc0af2ba7a0efea0261a53a0b93c1e021f7eb1ee
Greg Cain <codegregcode@gmail.com>
2022-12-08 16:11:04 +0000

    completes kata6

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,14 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const list = namesObj.map((object) => object.name);
+  const last = list.pop();
+  return `${list.join(", ")} & ${last}`;
+  //   const list = namesObj.reduce((acc, curr, index) => {
+//     if (index < namesObj.length - 1) {
+//       return `${acc}, ${curr.name}`;
+//     } 
+//     return `${acc} & ${curr.name}`;
+//   }, namesObj[0].name);
+//   return list;
+};
 
 module.exports = joinNames;
