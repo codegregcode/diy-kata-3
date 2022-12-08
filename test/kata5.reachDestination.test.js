e52a09b0efc87a413ca386685f6da7e5dfb59fa1
Greg Cain <codegregcode@gmail.com>
2022-12-08 12:04:56 +0000

    writes tests for kata5

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(50, 95)).toBe("I should be there in 0.5 hours.");
+    expect(reachDestination(20, 1.5)).toBe("I should be there in 13.5 hours.");
+  });
 });
