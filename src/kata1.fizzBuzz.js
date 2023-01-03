46c8dd3ec5f5df4704618bec6f895ea46118a662
Greg Cain <codegregcode@gmail.com>
2023-01-03 17:58:39 +0000

    adds code review suggestions

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,22 +1,18 @@
 const fizzBuzz = (number) => {
-const divide3 = (number % 3) === 0;
-const divide5 = (number % 5) === 0;
-// return divide3 ? 'Fizz' : divide5 ? 'Buzz' ? divide3 && divide5 ? 'FizzBuzz' : number;
-    // const divide3 = (number % 3) === 0;
-// const divide5 = (number % 5) === 0;
-
-if (divide3 && divide5) {
+  const divide3 = number % 3 === 0;
+  const divide5 = number % 5 === 0;
+  if (divide3 && divide5) {
     return 'FizzBuzz'
-}
+  }
 
-if (divide3) {
+  if (divide3) {
     return 'Fizz'
-}
+  }
 
-if (divide5) {
+  if (divide5) {
     return 'Buzz'
-}
-return number
-}
+  }
+  return number;
+};
 
 module.exports = fizzBuzz;
